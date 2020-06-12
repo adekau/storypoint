95ba344a2a91968718a7d438371242e907b3f01d
adekau <alex.potatofarms@gmail.com>
2020-06-12 18:57:25 -0400

    move functions to event module

--- server/event.ts
+++ server/event.ts
@@ -1,6 +1,11 @@
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+
+import { IRoom } from '../shared/types/room.ts';
 import { StoryPointEvent } from '../shared/types/story-point-event.ts';
-import { removeUser, roomsMap } from './data/data.ts';
+import { IUser } from '../shared/types/user.ts';
+import { addUserToRoom, removeUser, roomsMap, usersMap } from './data/data.ts';
 import Logger from './logger.ts';
+import { HandleEventArguments } from './room.ts';
 import { translateUsers } from './translation.ts';
 
 export async function emitEvent(roomId: string) {
@@ -21,5 +26,48 @@ export async function emitEvent(roomId: string) {
             await removeUser(user.userId);
             break;
         }
+    }
+}
+
+export async function userJoinEvent({ ev, userId, ws }: HandleEventArguments): Promise<void> {
+    if (ev.event !== 'join')
+        return;
+
+    const userJoin: IUser = {
+        roomId: ev.roomId,
+        websocket: ws,
+        userId: userId
+    };
+    usersMap.set(userId, userJoin);
+    await addUserToRoom(userJoin, ev.roomId);
+    
+    Logger.log(`User ${userJoin.userId} joined room ${userJoin.roomId}.`);
+
+    await emitEvent(ev.roomId);
+}
+
+export async function createRoomEvent({ ev, userId, ws }: HandleEventArguments): Promise<void> {
+    if (ev.event !== 'create')
+        return;
+    const roomId = v4.generate();
+    const room: IRoom = {
+        users: [],
+        roomName: ev.roomName,
+        id: roomId
+    };
+    roomsMap.set(roomId, room);
+    const event = {
+        event: 'roomCreate',
+        roomId,
+        roomName: room.roomName,
+        users: room.users
+    };
+
+    Logger.log(`Room ${roomId} created.`);
+
+    try {
+        await ws.send(JSON.stringify(event));
+    } catch (e) {
+        Logger.warn(`User ${userId} unable to be reached while creating room.`);
     }
 }
--- server/room.ts
+++ server/room.ts
@@ -1,14 +1,12 @@
 import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 import { isWebSocketCloseEvent, isWebSocketPingEvent, WebSocket } from 'https://deno.land/std/ws/mod.ts';
 
-import { IRoom } from '../shared/types/room.ts';
 import { StoryPointEvent } from '../shared/types/story-point-event.ts';
-import { IUser } from '../shared/types/user.ts';
-import { addUserToRoom, removeUser, roomsMap, usersMap } from './data/data.ts';
-import { emitEvent } from './event.ts';
+import { removeUser } from './data/data.ts';
+import { createRoomEvent, userJoinEvent } from './event.ts';
 import Logger from './logger.ts';
 
-type HandleEventArguments = { ev: StoryPointEvent, userId: string, ws: WebSocket };
+export type HandleEventArguments = { ev: StoryPointEvent, userId: string, ws: WebSocket };
 
 export default async function handle(ws: WebSocket) {
     const userId = v4.generate();
@@ -58,48 +56,5 @@ async function handleEvent({ ev, userId }: HandleEventArguments): Promise<void> 
             break;
         default:
             return;
-    }
-}
-
-async function userJoinEvent({ ev, userId, ws }: HandleEventArguments): Promise<void> {
-    if (ev.event !== 'join')
-        return;
-
-    const userJoin: IUser = {
-        roomId: ev.roomId,
-        websocket: ws,
-        userId: userId
-    };
-    usersMap.set(userId, userJoin);
-    await addUserToRoom(userJoin, ev.roomId);
-    
-    Logger.log(`User ${userJoin.userId} joined room ${userJoin.roomId}.`);
-
-    await emitEvent(ev.roomId);
-}
-
-async function createRoomEvent({ ev, userId, ws }: HandleEventArguments): Promise<void> {
-    if (ev.event !== 'create')
-        return;
-    const roomId = v4.generate();
-    const room: IRoom = {
-        users: [],
-        roomName: ev.roomName,
-        id: roomId
-    };
-    roomsMap.set(roomId, room);
-    const event = {
-        event: 'roomCreate',
-        roomId,
-        roomName: room.roomName,
-        users: room.users
-    };
-
-    Logger.log(`Room ${roomId} created.`);
-
-    try {
-        await ws.send(JSON.stringify(event));
-    } catch (e) {
-        Logger.warn(`User ${userId} unable to be reached while creating room.`);
     }
 }
