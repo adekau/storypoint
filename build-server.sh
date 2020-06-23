28411aca7f2c3216d64059c530c77f79104a813b
adekau <alex.potatofarms@gmail.com>
2020-06-23 01:48:39 -0400

    local deno

--- build-server.sh
+++ build-server.sh
@@ -4,15 +4,13 @@ DENO_VERSION=1.1.1
 
 [ ! -d "./server/dist" ] && mkdir server/dist
 
-apk add --virtual .download --no-cache curl 
 curl -fsSL https://github.com/denoland/deno/releases/download/v${DENO_VERSION}/deno-x86_64-unknown-linux-gnu.zip --output deno.zip
 unzip deno.zip
 rm deno.zip
 chmod 777 deno
-apk del .download
 
 # first rebuild the cache
-deno cache server/server.ts
-deno bundle server/server.ts server/dist/server.js
+./deno cache server/server.ts
+./deno bundle server/server.ts server/dist/server.js
 
 docker build -t dekau/storypoint-server ./server
