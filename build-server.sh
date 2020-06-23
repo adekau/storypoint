717ad2a48317afaf51a8a9d1a41df7ce7d40b71a
adekau <alex.potatofarms@gmail.com>
2020-06-23 01:33:16 -0400

    RUN was from docker

--- build-server.sh
+++ build-server.sh
@@ -4,17 +4,13 @@ DENO_VERSION=1.1.1
 
 [ ! -d "./server/dist" ] && mkdir server/dist
 
-# if deno doesn't exist, install it
-command -v deno >/dev/null && (
-    RUN apk add --virtual .download --no-cache curl \
-    && curl -fsSL https://github.com/denoland/deno/releases/download/v${DENO_VERSION}/deno-x86_64-unknown-linux-gnu.zip \
-            --output deno.zip \
-    && unzip deno.zip \
-    && rm deno.zip \
-    && chmod 777 deno \
-    && mv deno /bin/deno \
-    && apk del .download
-)
+apk add --virtual .download --no-cache curl 
+curl -fsSL https://github.com/denoland/deno/releases/download/v${DENO_VERSION}/deno-x86_64-unknown-linux-gnu.zip --output deno.zip
+unzip deno.zip
+rm deno.zip
+chmod 777 deno
+mv deno /bin/deno
+apk del .download
 
 # first rebuild the cache
 deno cache server/server.ts
