e48b3d1ad9d5d5c5ce36eaf7c8202452e940077a
adekau <alex.potatofarms@gmail.com>
2020-06-23 01:08:43 -0400

    deno doesn't exist, install it

--- build-server.sh
+++ build-server.sh
@@ -2,6 +2,18 @@
 
 [ ! -d "./server/dist" ] && mkdir server/dist
 
+# if deno doesn't exist, install it
+command -v deno >/dev/null && (
+    RUN apk add --virtual .download --no-cache curl \
+    && curl -fsSL https://github.com/denoland/deno/releases/download/v${DENO_VERSION}/deno-x86_64-unknown-linux-gnu.zip \
+            --output deno.zip \
+    && unzip deno.zip \
+    && rm deno.zip \
+    && chmod 777 deno \
+    && mv deno /bin/deno \
+    && apk del .download
+)
+
 # first rebuild the cache
 deno cache server/server.ts
 deno bundle server/server.ts server/dist/server.js
