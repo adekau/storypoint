489a3b688c573011a75fc48c1afd31a84e5a32e1
Alex Dekau <alex.potatofarms@gmail.com>
2020-06-26 03:29:24 -0400

    update redis deployment variables

--- deploy-redis.sh
+++ deploy-redis.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-helm install stable/redis-ha --name storypoint-redis
+helm install stable/redis-ha --name storypoint-redis --set replicas=2 --set haproxy.enabled=true --set haproxy.replicas=2
